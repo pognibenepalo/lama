dfa4a12aacbbe277108fa5661725b9c757343995
kittipon kanda <Demonxpzero@gmail.com>
2020-03-13 11:37:25 +0700

    update url for ios

--- frontend/lib/constants.dart
+++ frontend/lib/constants.dart
@@ -1,3 +1,7 @@
+import 'dart:io' show Platform;
+
 class Constants {
-  static final String apiServer = 'http://10.0.2.2:7070/api/v1';
+  static final String apiServer = Platform.isAndroid
+      ? 'http://10.0.2.2:7070/api/v1'
+      : 'http://localhost:7070/api/v1';
 }

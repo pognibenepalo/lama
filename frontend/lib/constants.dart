06c7003b1338a85f7efef87c0085bda7532f1810
kubooz <kubooz.kk@gmail.com>
2020-03-13 12:29:29 +0700

    update test

--- frontend/lib/constants.dart
+++ frontend/lib/constants.dart
@@ -1,9 +1,9 @@
 import 'dart:io' show Platform;
 
 class Constants {
-//  static final String apiServer = Platform.isAndroid
-//      ? 'http://10.0.2.2:7070/api/v1'
-//      : 'http://localhost:7070/api/v1';
+  static final String apiServer = Platform.isAndroid
+      ? 'http://10.0.2.2:7070/api/v1'
+      : 'http://localhost:7070/api/v1';
 
-  static final String apiServer = 'http://localhost:7070/api/v1';
+//  static final String apiServer = 'http://localhost:7070/api/v1';
 }
